294f7fec6254bb63f623e6de90eaab5b15192ca5
GerQuil <quillosageremy@gmail.com>
2022-05-20 02:15:54 +0800

    FIX:EmployeeTable

--- Employee/employee.php
+++ Employee/employee.php
@@ -16,6 +16,10 @@
     $holiday = "";
     $location = "";
     $date = "";
+    $bonus = "";
+    $hazard = 0;
+    $hazardpay = 0;
+    $ctr = 0;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,6 +39,7 @@
         <div class="text-end">
             <b style="position: relative; top: 20px;">Filter by Month:</b>
             <select name="months" id="months">
+                <option value="All">Show All Records</option>
                 <option value="Jan">January</option>
                 <option value="Feb">February</option>
                 <option value="Mar">March</option>
@@ -47,8 +52,8 @@
                 <option value="Oct">October</option>
                 <option value="Nov">November</option>
                 <option value="Dec">December</option>
-                <option value="All">Show All Records</option>
-            </select>
+            </select><br>
+            
         </div>
         <?php 
             if($connect){
@@ -58,8 +63,9 @@
                 JOIN team ON attendance.teamID = team.teamID
                 JOIN teamlead ON attendance.leadID = teamlead.leadID
                 JOIN paidtimeoff ON attendance.paidTimeOffID = paidtimeoff.paidTimeOffID
-                JOIN shiftschedule ON attendance.scheduleID = shiftschedule.scheduleID
-                JOIN hybridschedule ON attendance.hybridSchedID = hybridschedule.hybridScheduleID";
+                JOIN hybridschedule ON attendance.hybridSchedID = hybridschedule.hybridScheduleID
+                JOIN hazardpay ON attendance.workType = hazardpay.Area
+                WHERE employee.empID = attendance.empID";
                 $query = mysqli_query($connect,$sql);
                 if(mysqli_num_rows($query) > 0){
                     while($r = mysqli_fetch_assoc($query)){
@@ -72,36 +78,30 @@
                         $pto = $r['paidTimeOffName'];
                         $holiday = $r['holiday'];
                         $location = $r['location'];
-                        $shiftstart = date("h:i a", strtotime($r['startTime']));
-                        $shiftend = date("h:i a", strtotime($r['endTime']));
+                        $shiftstart = date("h:i a", strtotime($r['start_time']));
+                        $shiftend = date("h:i a", strtotime($r['end_time']));
                         $teamhybrid = $r['hybridName'];
                         $date = date("M j, Y", strtotime($r['attendanceStamp']));
+                        if($worktype != "WFH"){
+                            $ctr++;  
+                            $hazard += $r['harzardPay'];  
+                        }
                         
                         echo "<div class='employee-card' id='info-tile'>";
                             echo "<div style='display: flex'>";
-                                echo "<div class='text-start' style='position: relative; margin: auto; top: 10px;  width:30%;'>";
+                                echo "<div style='position: relative; margin: auto; top: 10px;  width:30%;'>";
                                     echo "<b>Department Name:</b>";
                                     echo "<p id='department'>$department</p>";
                                 echo "</div>";
-                                echo "<div class='text-center' style='position: relative; margin: auto; top: 10px;  width:30%;'>";
+                                echo "<div style='position: relative; margin: auto; top: 10px;  width:30%;'>";
                                     echo "<b>Team Name:</b>";
                                     echo "<p id='team'>$team</p>";
                                 echo "</div>";
-                                echo "<div class='text-end' style='position: relative; margin: auto; top: 10px;  width:30%;'>";
+                                echo "<div style='position: relative; margin: auto; top: 10px;  width:30%;'>";
                                     echo "<b>Lead Name:</b>";
                                     echo "<p id='lead'>$leadfirst $leadmid $leadlast</p>";
                                 echo "</div>";
-                            echo "</div>";
-                            echo "<div style='display: flex; margin-top: 50px;'>";
-                                echo "<div class='text-start' style='position: relative; margin: auto; top: 10px; width:30%;'>";
-                                    echo "<b>Accumulated Pro-Rated Attendance Bonus:</b>";
-                                    echo "<p id='department'>$department</p>";
-                                echo "</div>";
-                                echo '<div class="text-center" style="position: relative; margin: auto; top: 10px; width:30%;">';
-                                    echo '<b>Accumulated Pro-Rated Hazard Pay:</b>';
-                                    echo "<p id='department'>$department</p>";
-                                echo "</div>";
-                                echo '<div class="text-end" style="position: relative; margin: auto; top: 10px;  width:30%;">';
+                                echo '<div style="position: relative; margin: auto; top: 10px;  width:30%;">';
                                     echo '<b>Date:</b>';
                                     echo "<p id='date' class='date''>$date</p>";
                                 echo "</div>";
@@ -121,15 +121,24 @@
                             echo "<tr>";
                             echo "<td>$shiftstart - $shiftend</td>";
                             echo "<td>$teamhybrid</td>";
-                            echo "<td>$worktype</td>";
+                            echo "<td class='worktype'>$worktype</td>";
                             echo "<td>$pto</td>";
                             echo "<td>$holiday</td>";
                             echo "<td>$location</td>";
                             echo "</tr>";
                             echo "</tbody>";
                             echo "</table>";
-                        echo "</div><br>";
+                        echo "</div><br><br>";
+
                     }
+                    $bonus = 1000 / 20 * $ctr;
+                    $hazard *= $ctr;
+                    //hazardpay *= $ctr;
+                    
+                    echo "<b>Accumulated Pro-Rated Attendance Bonus:</b>";
+                    echo "<p class='bonus' style='color:green; font-weight:bold;'>Php $bonus.00</p>";
+                    echo "<b>Accumulated Pro-Rated Hazard Pay:</b>";
+                    echo "<p class='hazard'  style='color:green; font-weight:bold;'>Php $hazard.00</p>";
                 }
             } 
         ?>
@@ -139,11 +148,38 @@
 <script>
     $(document).on("change", "#months", function(){
         mon=$(this).val();
+        var sumofworkdays = 0;
+        var monthbonus = 0;
+        var monthhazard = 0;
         if(mon == 'All'){
             $('.employee-card').removeClass('d-none');
+            location.reload();
         }else{
             $('.employee-card').removeClass('d-none');
             $('.employee-card').find('.date:not(:contains("'+mon+'"))').parent().parent().parent().addClass('d-none');
+            
+            $('.employee-card').find('.date:contains("'+mon+'")').parent().parent().parent().find('.worktype').each(function(){
+            var worktypedata = $(this).html();
+                if(worktypedata != 'WFH'){
+                    sumofworkdays++; 
+                } 
+                if(worktypedata == 'Area 1'){
+                    monthhazard += 50;
+                }
+                if(worktypedata == 'Area 2'){
+                    monthhazard += 100;
+                }
+                if(worktypedata == 'Area 3'){
+                    monthhazard += 125;
+                }
+                if(worktypedata == 'Area 4'){
+                    monthhazard += 150;
+                }
+            });
+            var totmonthhazard = monthhazard * sumofworkdays;
+            $('.hazard').text('Php '+totmonthhazard+'.00');
+            monthbonus = 1000/20 * sumofworkdays;
+            $('.bonus').text('Php '+monthbonus+'.00');
         }
     });
 </script>
